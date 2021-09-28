946a990333fed299c89097bfca3b928be6d1998e
TORMOND <55413781+TORMOND@users.noreply.github.com>
2021-09-28 13:35:05 +0300

    Add files via upload

---
+++ zalisha.js
@@ -0,0 +1,54 @@
+//const slides = document.querySelector('.mySlides');
+//console.log(slides);
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 3000); // Change image every 5 seconds
+  
+}
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slide = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slide.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slide.length}
+  for (i = 0; i < slide.length; i++) {
+      slide[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slide[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+
+function toggle() {
+  var bar = document.getElementsByClassName("form");
+  bar.toggle("show");
+}
+document.getElementsByClassName("element").onclick = function () { toggle() };
+
+console.log('toggle')
